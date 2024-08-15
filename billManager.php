a6d822ad14958baa1cd885e8dd6d731690394f2f
kimpaolo25 <kimpaolocuenca03@gmail.com>
2024-08-15 12:37:17 +0800

    add sweetalert

--- billManager.php
+++ billManager.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Admin Dashboard</title>
     <link rel="stylesheet" href="./css/billManager.css">
+    <script src="./Javascript/sweetalert.js"></script>
 </head>
 <body>
     <header>
