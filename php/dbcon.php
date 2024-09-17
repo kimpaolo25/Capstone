8cb7a77ec5f6b1b38cefb48696b034a5c77cd511
kimpaolo25 <kimpaolocuenca03@gmail.com>
2024-09-17 14:48:32 +0800

    try db for hosting

--- php/dbcon.php
+++ php/dbcon.php
@@ -1,8 +1,8 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "prwai_data";
+$servername = getenv('DB_HOST');   // e.g., 'your-database-host'
+$username = getenv('DB_USER');     // e.g., 'your-database-user'
+$password = getenv('DB_PASS'); // e.g., 'your-database-password'
+$dbname = getenv('DB_NAME');       // e.g., 'your-database-name'
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -11,5 +11,3 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
-
-
