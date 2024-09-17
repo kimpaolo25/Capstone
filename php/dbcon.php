b62e5876254b914f27074062712b0fbff0c76767
kimpaolo25 <kimpaolocuenca03@gmail.com>
2024-09-17 14:58:01 +0800

    another login try for hosting

--- php/dbcon.php
+++ php/dbcon.php
@@ -1,8 +1,8 @@
 <?php
-$servername = getenv('DB_HOST');   // e.g., 'your-database-host'
-$username = getenv('DB_USER');     // e.g., 'your-database-user'
-$password = getenv('DB_PASS'); // e.g., 'your-database-password'
-$dbname = getenv('DB_NAME');       // e.g., 'your-database-name'
+$servername = "renderhost";
+$username = "capstone";
+$password = "12345";
+$dbname = "prwai_data";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -11,3 +11,5 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
+
